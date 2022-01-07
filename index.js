0d13826003b5a0ea1175ce52807fa8a22c0abca7
samuel-santinelli <samuelsantinelli@gmail.com>
2022-01-07 12:41:56 -0300

    aa

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+
+const app = express();
+
+const path = require('path');
+const router = express.Router();
+
+
+router.get('/sobre',function(req,res){
+    res.sendFile(path.join(__dirname +'/sobre.html'));
+
+
+})
+
+app.use('/',router);
+
+app.listen(process.env.port || 3000);
+
+console.log("Server rodando perfeitamente")
+
+
+// Servidor 
+
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req,res) => {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Conexão realizada com sucesso')
+
+});
+
+server.listen(port,hostname, () => {
+    console.log('Servidor rodando com sucesso')
+});
